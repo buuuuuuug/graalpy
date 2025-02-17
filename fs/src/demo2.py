002e4453605e58ea8b367176c3f8c35d514fe364
chaneychan <channarry@163.com>
2025-02-17 22:11:21 +0800

    sqlite hibernate

--- fs/src/demo2.py
+++ fs/src/demo2.py
@@ -1,1 +1,8 @@
-print("aaaaaaaaaaaaaaaaaaaa")
+import pandas as pd
+
+# 创建一个简单的 DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]}
+df = pd.DataFrame(data)
+
+# 显示 DataFrame
+print(df)
--- src/main/java/com/chaney/infra/graalpy/GraalPyApplication.java
+++ src/main/java/com/chaney/infra/graalpy/GraalPyApplication.java
@@ -2,8 +2,10 @@ package com.chaney.infra.graalpy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(DatabaseRuntimeHintsRegistrar.class)
 public class GraalPyApplication {
 
     public static void main(String[] args) {
